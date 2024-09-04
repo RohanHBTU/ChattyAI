a6396e819193cc7ca1d49d302e5a87883825820e
Rohan Kumar Singh <rohank587@gmail.com>
2024-09-04 16:43:26 -0400

    removed unwanted modules

--- shell_scripts/config.sh
+++ shell_scripts/config.sh
@@ -10,6 +10,4 @@ TMP_WORK_DIR="/scratch/users/$USERID"
 FRAME_DIR="/mnt/rds/redhen/gallina/projects/ChattyAI/FramesConstructions/fndata-1.7/frame"
 
 module load Python/3.11.3
-module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
-module load PyYAML/6.0-GCCcore-12.3.0
 
